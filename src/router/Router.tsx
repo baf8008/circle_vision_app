3e13e586608d0d2f9ac492a674390085fc64d737
baf8008 <baf0618@gmail.com>
2021-07-22 11:32:36 +0900

    すべてのroutingの処理。 Home配下のみheaderLayoutを追加

---
+++ src/router/Router.tsx
@@ -0,0 +1,40 @@
+import { Switch, Route } from 'react-router-dom';
+import { memo, VFC } from 'react';
+
+import { Login } from '../components/pages/Login';
+import { homeRoutes } from './HomeRoutes';
+import { SignUp } from '../components/pages/SignUp';
+import { Page404 } from '../components/pages/Page404';
+import { HeaderLayout } from '../components/templates/HeaderLayout';
+
+export const Router: VFC = memo(() => {
+	return (
+		<Switch>
+			<Route exact path='/'>
+				<Login />
+			</Route>
+			<Route exact path='/signup'>
+				<SignUp />
+			</Route>
+			<Route
+				path='/home'
+				render={({ match: { url } }) => (
+					<Switch>
+						{homeRoutes.map((route) => (
+							<Route
+								key={route.path}
+								exact={route.exact}
+								path={`${url}${route.path}`}
+							>
+								<HeaderLayout>{route.children}</HeaderLayout>
+							</Route>
+						))}
+					</Switch>
+				)}
+			/>
+			<Route path='*'>
+				<Page404 />
+			</Route>
+		</Switch>
+	);
+});
