e29ce989359130b02c9a55b0d7eb4d6b9fe14adb
baf8008 <baf0618@gmail.com>
2021-07-23 15:07:16 +0900

    styleのshadowを追加

--- src/components/atoms/button/PrimaryButton.tsx
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -2,16 +2,16 @@ import { VFC, memo, ReactNode } from 'react';
 
 import { Button } from '@chakra-ui/react';
 
-type Props={
-  onClick:()=>void;
-  children:ReactNode
-}
+type Props = {
+	onClick: () => void;
+	children: ReactNode;
+};
 
 export const PrimaryButton: VFC<Props> = memo((props) => {
-  const{onClick,children}=props
+	const { onClick, children } = props;
 	return (
-		<Button colorScheme='blue' size='md' onClick={onClick} >
-		{children}
+		<Button colorScheme='blue' size='md' shadow='md' onClick={onClick}>
+			{children}
 		</Button>
 	);
 });
