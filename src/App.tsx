f86f1770cfe8a4fca2918dbe0269af56ed1d0222
baf8008 <baf0618@gmail.com>
2021-07-28 15:56:02 +0900

    グローバルステートの処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 import globalTheme from './theme/GlobalTheme';
 import { Router } from './router/Router';
+import { AuthProvider } from './components/providers/AuthProvider';
 
 export const App = () => {
 	return (
 		<ChakraProvider theme={globalTheme}>
-			<BrowserRouter>
-				<Router />
-			</BrowserRouter>
+			<AuthProvider>
+				<BrowserRouter>
+					<Router />
+				</BrowserRouter>
+			</AuthProvider>
 		</ChakraProvider>
 	);
 };
