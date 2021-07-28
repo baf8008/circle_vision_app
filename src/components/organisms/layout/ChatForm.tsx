9c47008472c64b9502552f2449fc37ab57ac9cfd
baf8008 <baf0618@gmail.com>
2021-07-28 15:55:29 +0900

    入力されたコメントを追加する処理を追加

--- src/components/organisms/layout/ChatForm.tsx
+++ src/components/organisms/layout/ChatForm.tsx
@@ -1,22 +1,48 @@
-import { memo, VFC } from "react";
+import { memo, VFC, useState, FormEvent, useCallback } from 'react';
 
-import { Button, FormControl, Textarea } from "@chakra-ui/react";
+import { Button, FormControl, Textarea } from '@chakra-ui/react';
 
-export const ChatForm:VFC=memo(()=>{
-  return (
-		<FormControl
-			m={2}
-			display='flex'
-			justifyContent='space-around'
-			alignItems='center'
-		>
-			<Textarea
-				placeholder='チャットを入力....'
-				w={{ base: '70%', md: '85%' }}
-			/>
-			<Button mr={{ base: '15px', md: '30px' }} colorScheme='pink' shadow='md'>
-				送信
-			</Button>
-		</FormControl>
+type Props = {
+	addChat: (text: string) => void;
+};
+
+export const ChatForm: VFC<Props> = memo((props) => {
+	const { addChat } = props;
+	const [text, setText] = useState('');
+
+	// 入力されたコメントを追加する処理
+	const handleSubmit = useCallback(
+		(e: FormEvent<HTMLFormElement>) => {
+			e.preventDefault();
+			addChat(text);
+			setText('');
+		},
+		[text, addChat]
 	);
-})
+	return (
+		<form onSubmit={handleSubmit}>
+			<FormControl
+				m={2}
+				display='flex'
+				justifyContent='space-around'
+				alignItems='center'
+			>
+				<Textarea
+					value={text}
+					onChange={(e) => setText(e.target.value)}
+					placeholder='チャットを入力....'
+					w={{ base: '70%', md: '85%' }}
+				/>
+				<Button
+					type='submit'
+					mr={{ base: '15px', md: '30px' }}
+					colorScheme='pink'
+					shadow='md'
+					disabled={text.trim() === ''}
+				>
+					送信
+				</Button>
+			</FormControl>
+		</form>
+	);
+});
