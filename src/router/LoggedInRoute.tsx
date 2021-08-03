9906fbba444dffb455b0b9fed80618e7a455aa95
baf8008 <baf0618@gmail.com>
2021-08-03 18:02:57 +0900

    ログアウトしたら、login画面に遷移する処理を追加

--- src/router/LoggedInRoute.tsx
+++ src/router/LoggedInRoute.tsx
@@ -1,7 +1,7 @@
 import { memo, VFC, useContext, ReactNode } from 'react';
 import { Redirect } from 'react-router-dom';
 
-import { Spinner } from '@chakra-ui/react';
+import { Flex, Spinner, Text } from '@chakra-ui/react';
 
 import { AuthContext } from '../components/providers/AuthProvider';
 
@@ -12,19 +12,27 @@ type Props = {
 export const LoggedInRoute: VFC<Props> = memo((props) => {
 	const { children } = props;
 	const authState = useContext(AuthContext);
+
 	if (authState.loading) {
 		return (
-			<Spinner
-				thickness='4px'
-				speed='0.65s'
-				emptyColor='gray.200'
-				color='blue.500'
-				size='xl'
-			/>
+			<Flex align='center' justify='start'>
+				<Spinner
+					thickness='4px'
+					speed='0.65s'
+					emptyColor='gray.200'
+					color='blue.500'
+					size='xl'
+				/>
+				<Text color='blue.500' fontSize='3xl'>
+					ロード中・・・・
+				</Text>
+			</Flex>
 		);
 	}
+
 	if (authState.user === null) {
 		return <Redirect to='/' />;
 	}
+
 	return <>{children}</>;
 });
