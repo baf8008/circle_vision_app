cb37ae652803b1ed07639274d560a1324f6e4b28
baf8008 <baf0618@gmail.com>
2021-09-03 13:43:33 +0900

    アプリ紹介画面

--- src/components/pages/AppInfo.tsx
+++ src/components/pages/AppInfo.tsx
@@ -1,5 +1,9 @@
-import { VFC, memo } from 'react';
+import { VFC, memo, useContext } from 'react';
+
+import { AuthContext } from '../providers/AuthProvider';
 
 export const AppInfo: VFC = memo(() => {
+		const value = useContext(AuthContext);
+		console.log(value);
 	return <p>アプリ紹介</p>;
 });
