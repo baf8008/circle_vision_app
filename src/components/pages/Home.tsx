59442153fbd193668c18a8bff0998fc17a48fafb
baf8008 <baf0618@gmail.com>
2021-09-03 13:42:45 +0900

    ホーム画面

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,9 @@
-import { VFC, memo } from 'react';
+import { VFC, memo, useContext } from 'react';
+
+import { AuthContext } from '../providers/AuthProvider';
 
 export const Home: VFC = memo(() => {
+	const value = useContext(AuthContext);
+	console.log(value);
 	return <p>ホームページ</p>;
 });
